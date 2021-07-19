8f4d4c9ad25660d51d1ef89303cbda0e307e0858
FranciszekCzajka <franekczajkaa@gmail.com>
2021-07-19 19:31:57 +0200

    Standardise receiving data

--- main.js
+++ main.js
@@ -7,17 +7,18 @@ $(function (){
         list.empty();
         $.ajax({
             type: 'GET',
-            url: 'https://api.nal.usda.gov/fdc/v1/food/' + ID + '%20?api_key=nBKTTixEru2BA0nzf0DBnapEY5Tc6iERJ3lXpUU8',
+            url: 'https://api.nal.usda.gov/fdc/v1/food/' + ID + '?nutrients=https://api.nal.usda.gov/fdc/v1/food/1102644?nutrients=208&nutrients=203&nutrients=204&nutrients=205&nutrients=291&nutrients=269&api_key=nBKTTixEru2BA0nzf0DBnapEY5Tc6iERJ3lXpUU8&nutrients=201&nutrients=202&nutrients=203&nutrients=204&nutrients=205&nutrients=206&nutrients=207&nutrients=208&nutrients=209&nutrients=210&api_key=nBKTTixEru2BA0nzf0DBnapEY5Tc6iERJ3lXpUU8',
             success: function(data) {
+                console.log(data);
                 list.append('<h1>Food: ' + data.description + '</h1>');
                 list.append('<ul class="nutrients"></ul>');
                 var nutrients = $('.nutrients');
+                nutrients.append('<li>' + data.foodNutrients[0].nutrient.name + " " + data.foodNutrients[0].amount + " " + data.foodNutrients[0].nutrient.unitName + '</li>');
+                nutrients.append('<li>' + data.foodNutrients[1].nutrient.name + " " + data.foodNutrients[1].amount + " " + data.foodNutrients[1].nutrient.unitName + '</li>');
                 nutrients.append('<li>' + data.foodNutrients[2].nutrient.name + " " + data.foodNutrients[2].amount + " " + data.foodNutrients[2].nutrient.unitName + '</li>');
+                nutrients.append('<li>' + data.foodNutrients[3].nutrient.name + " " + data.foodNutrients[3].amount + " " + data.foodNutrients[3].nutrient.unitName + '</li>');
                 nutrients.append('<li>' + data.foodNutrients[4].nutrient.name + " " + data.foodNutrients[4].amount + " " + data.foodNutrients[4].nutrient.unitName + '</li>');
                 nutrients.append('<li>' + data.foodNutrients[5].nutrient.name + " " + data.foodNutrients[5].amount + " " + data.foodNutrients[5].nutrient.unitName + '</li>');
-                nutrients.append('<li>' + data.foodNutrients[8].nutrient.name + " " + data.foodNutrients[8].amount + " " + data.foodNutrients[8].nutrient.unitName + '</li>');
-                nutrients.append('<li>' + data.foodNutrients[10].nutrient.name + " " + data.foodNutrients[10].amount + " " + data.foodNutrients[10].nutrient.unitName + '</li>');
-                nutrients.append('<li>' + data.foodNutrients[9].nutrient.name + " " + data.foodNutrients[9].amount + " " + data.foodNutrients[9].nutrient.unitName + '</li>');
             }
         });
     }
