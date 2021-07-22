3b840aba5e2222f75e5d97290df16bfe856ef6fe
FranciszekCzajka <franekczajkaa@gmail.com>
2021-07-22 15:13:08 +0200

    Style results

--- main.js
+++ main.js
@@ -1,44 +1,49 @@
 $(function (){
 
-    var list = $('.list');
-    var results = $('.results-names');
+    var results = $('.results');
     
     function specificFoodInfo(ID) {
-        list.empty();
         $.ajax({
             type: 'GET',
-            url: 'https://api.nal.usda.gov/fdc/v1/food/' + ID + '?nutrients=https://api.nal.usda.gov/fdc/v1/food/1102644?nutrients=208&nutrients=203&nutrients=204&nutrients=205&nutrients=291&nutrients=269&api_key=nBKTTixEru2BA0nzf0DBnapEY5Tc6iERJ3lXpUU8&nutrients=201&nutrients=202&nutrients=203&nutrients=204&nutrients=205&nutrients=206&nutrients=207&nutrients=208&nutrients=209&nutrients=210&api_key=nBKTTixEru2BA0nzf0DBnapEY5Tc6iERJ3lXpUU8',
+            url: 'https://api.nal.usda.gov/fdc/v1/food/' + ID + '?nutrients=203&nutrients=204&nutrients=205&nutrients=208&nutrients=291&nutrients=269&api_key=nBKTTixEru2BA0nzf0DBnapEY5Tc6iERJ3lXpUU8',
             success: function(data) {
-                console.log(data);
-                list.append('<h1>Food: ' + data.description + '</h1>');
-                list.append('<ul class="nutrients"></ul>');
+                results.append('<h3 class="food-name">Food: ' + data.description + '</h3>');
+                results.append('<table class="nutrients"></table>');
                 var nutrients = $('.nutrients');
-                nutrients.append('<li>' + data.foodNutrients[0].nutrient.name + " " + data.foodNutrients[0].amount + " " + data.foodNutrients[0].nutrient.unitName + '</li>');
-                nutrients.append('<li>' + data.foodNutrients[1].nutrient.name + " " + data.foodNutrients[1].amount + " " + data.foodNutrients[1].nutrient.unitName + '</li>');
-                nutrients.append('<li>' + data.foodNutrients[2].nutrient.name + " " + data.foodNutrients[2].amount + " " + data.foodNutrients[2].nutrient.unitName + '</li>');
-                nutrients.append('<li>' + data.foodNutrients[3].nutrient.name + " " + data.foodNutrients[3].amount + " " + data.foodNutrients[3].nutrient.unitName + '</li>');
-                nutrients.append('<li>' + data.foodNutrients[4].nutrient.name + " " + data.foodNutrients[4].amount + " " + data.foodNutrients[4].nutrient.unitName + '</li>');
-                nutrients.append('<li>' + data.foodNutrients[5].nutrient.name + " " + data.foodNutrients[5].amount + " " + data.foodNutrients[5].nutrient.unitName + '</li>');
+                nutrients.append('<tr class="nutrients-row"><th class="nutrients-header">' + 'Name' + '</th><th class="nutrients-header">' + 'Amount' + '</th><th class="nutrients-header">' + 'Unit' + '</th></tr>');
+                nutrients.append('<tr class="nutrients-row"><td class="nutrients-cell">' + data.foodNutrients[0].nutrient.name + '</td><td class="nutrients-cell">' + data.foodNutrients[0].amount + '</td><td class="nutrients-cell unit">' + data.foodNutrients[0].nutrient.unitName + '</td></tr>');
+                nutrients.append('<tr class="nutrients-row"><td class="nutrients-cell">' + data.foodNutrients[1].nutrient.name + '</td><td class="nutrients-cell">' + data.foodNutrients[1].amount + '</td><td class="nutrients-cell unit">' + data.foodNutrients[1].nutrient.unitName + '</td></tr>');
+                nutrients.append('<tr class="nutrients-row"><td class="nutrients-cell">' + data.foodNutrients[2].nutrient.name + '</td><td class="nutrients-cell">' + data.foodNutrients[2].amount + '</td><td class="nutrients-cell unit">' + data.foodNutrients[2].nutrient.unitName + '</td></tr>');
+                nutrients.append('<tr class="nutrients-row"><td class="nutrients-cell">' + 'Carbohydrates' + '</td><td class="nutrients-cell">' + data.foodNutrients[3].amount + '</td><td class="nutrients-cell unit">' + data.foodNutrients[3].nutrient.unitName + '</td></tr>');
+                nutrients.append('<tr class="nutrients-row"><td class="nutrients-cell">' + 'Sugars' + '</td><td class="nutrients-cell">' + data.foodNutrients[5].amount + '</td><td class="nutrients-cell unit">' + data.foodNutrients[5].nutrient.unitName + '</td></tr>');
+                nutrients.append('<tr class="nutrients-row"><td class="nutrients-cell">' + data.foodNutrients[4].nutrient.name + '</td><td class="nutrients-cell">' + data.foodNutrients[4].amount + '</td><td class="nutrients-cell unit">' + data.foodNutrients[4].nutrient.unitName + '</td></tr>');
             }
         });
     }
     $(".submit").click(function() {
-        list.empty();
-        var input = $(".input").val();
+        if($(".input").val().length > 0) {
+            var input = $(".input").val();
+            if($(".food-title").length) {
+                $(".food-title").remove();
+                $(".nutrients").remove();
+            }
+        }
         $.ajax({                
             type: 'GET',
             input: {"data": input},
             url: 'https://api.nal.usda.gov/fdc/v1/foods/search?query=' + input + '&dataType=Survey%20%28FNDDS%29&pageSize=200&pageNumber=1&sortBy=dataType.keyword&api_key=nBKTTixEru2BA0nzf0DBnapEY5Tc6iERJ3lXpUU8',
             success: function(data) {
                 results.empty();
+                results.append('<ul class="results-list"></ul>');
+                var resultsList = $('.results-list');
 
                 for(var counter = 0; counter < 10; counter++) {
-                    results.append('<li class="result" value="' + counter + '">' + data.foods[counter].description + '</li>');
+                    resultsList.append('<li class="result" value="' + counter + '">' + data.foods[counter].description + '</li>');
                 }
 
                 $(".result").click(function() {
-                    var temp = $(this).val();
                     results.empty();
+                    var temp = $(this).val();
                     specificFoodInfo(data.foods[temp].fdcId);
                 });
             }
