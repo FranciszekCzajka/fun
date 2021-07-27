54f1d348ff34920ec96327b83564a7729f7d3fcc
FranciszekCzajka <franekczajkaa@gmail.com>
2021-07-27 20:30:24 +0200

    Make minor changes to make code more mordern like: var -> let / const

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 $(function (){
 
-    var results = $('.results');
+    const results = $('.results');
 
     function error(errorText) {
-        var landing = $('.landing');
+        const landing = $('.landing');
         landing.after('<div class="connection-error"></div>')
-        var connectionError = $('.connection-error');
+        const connectionError = $('.connection-error');
         connectionError.append('<div class="connection-modal"></div>');
-        var connectionModal = $('.connection-modal');
+        const connectionModal = $('.connection-modal');
         connectionModal.append('<div class="connection-header"></div>');
         connectionModal.append('<div class="connection-message"><h4>' + errorText + '</h4></div>');
-        var connectionHeader = $('.connection-header');
+        const connectionHeader = $('.connection-header');
         connectionHeader.append('<div class="connection-header-left">Error!</div><div class="connection-header-right"><div class="connection-x"><i class="fas fa-times"></i></div></div>');
         $('.connection-x').click(function() {
             connectionError.remove();
@@ -18,28 +18,30 @@ $(function (){
     }
     
     function specificFoodInfo(ID) {
+        results.empty();
         $.ajax({
             type: 'GET',
             url: 'https://api.nal.usda.gov/fdc/v1/food/' + ID + '?nutrients=203&nutrients=204&nutrients=205&nutrients=208&nutrients=291&nutrients=269&api_key=nBKTTixEru2BA0nzf0DBnapEY5Tc6iERJ3lXpUU8',
             success: function(data) {
                 results.append('<h3 class="food-name">Food: ' + data.description + '</h3>');
                 results.append('<table class="nutrients"></table>');
-                var nutrients = $('.nutrients');
+                const nutrients = $('.nutrients');
                 nutrients.append('<tr class="nutrients-row"><th class="nutrients-header">' + 'Name' + '</th><th class="nutrients-header">' + 'Amount (per 100g)' + '</th><th class="nutrients-header">' + 'Unit' + '</th></tr>');
-                nutrients.append('<tr class="nutrients-row"><td class="nutrients-cell">' + data.foodNutrients[0].nutrient.name + '</td><td class="nutrients-cell amount">' + data.foodNutrients[0].amount + '</td><td class="nutrients-cell unit">' + data.foodNutrients[0].nutrient.unitName + '</td></tr>');
-                nutrients.append('<tr class="nutrients-row"><td class="nutrients-cell">' + data.foodNutrients[1].nutrient.name + '</td><td class="nutrients-cell amount">' + parseFloat(data.foodNutrients[1].amount).toFixed(2) + '</td><td class="nutrients-cell unit">' + data.foodNutrients[1].nutrient.unitName + '</td></tr>');
-                nutrients.append('<tr class="nutrients-row"><td class="nutrients-cell">' + data.foodNutrients[2].nutrient.name + '</td><td class="nutrients-cell amount">' + parseFloat(data.foodNutrients[2].amount).toFixed(2) + '</td><td class="nutrients-cell unit">' + data.foodNutrients[2].nutrient.unitName + '</td></tr>');
-                nutrients.append('<tr class="nutrients-row"><td class="nutrients-cell">' + 'Carbohydrates' + '</td><td class="nutrients-cell amount">' + parseFloat(data.foodNutrients[3].amount).toFixed(2) + '</td><td class="nutrients-cell unit">' + data.foodNutrients[3].nutrient.unitName + '</td></tr>');
-                nutrients.append('<tr class="nutrients-row"><td class="nutrients-cell">' + 'Sugars' + '</td><td class="nutrients-cell amount">' + parseFloat(data.foodNutrients[5].amount).toFixed(2) + '</td><td class="nutrients-cell unit">' + data.foodNutrients[5].nutrient.unitName + '</td></tr>');
-                nutrients.append('<tr class="nutrients-row"><td class="nutrients-cell">' + data.foodNutrients[4].nutrient.name + '</td><td class="nutrients-cell amount">' + parseFloat(data.foodNutrients[4].amount).toFixed(2) + '</td><td class="nutrients-cell unit">' + data.foodNutrients[4].nutrient.unitName + '</td></tr>');
+                nutrients.append('<tr class="nutrients-row"><td class="nutrients-cell">' + data.foodNutrients[0].nutrient.name + '</td><td class="nutrients-cell nutrients-cell-amount">' + data.foodNutrients[0].amount + '</td><td class="nutrients-cell nutrients-cell-unit">' + data.foodNutrients[0].nutrient.unitName + '</td></tr>');
+                nutrients.append('<tr class="nutrients-row"><td class="nutrients-cell">' + data.foodNutrients[1].nutrient.name + '</td><td class="nutrients-cell nutrients-cell-amount">' + parseFloat(data.foodNutrients[1].amount).toFixed(2) + '</td><td class="nutrients-cell nutrients-cell-unit">' + data.foodNutrients[1].nutrient.unitName + '</td></tr>');
+                nutrients.append('<tr class="nutrients-row"><td class="nutrients-cell">' + data.foodNutrients[2].nutrient.name + '</td><td class="nutrients-cell nutrients-cell-amount">' + parseFloat(data.foodNutrients[2].amount).toFixed(2) + '</td><td class="nutrients-cell nutrients-cell-unit">' + data.foodNutrients[2].nutrient.unitName + '</td></tr>');
+                nutrients.append('<tr class="nutrients-row"><td class="nutrients-cell">' + 'Carbohydrates' + '</td><td class="nutrients-cell nutrients-cell-amount">' + parseFloat(data.foodNutrients[3].amount).toFixed(2) + '</td><td class="nutrients-cell nutrients-cell-unit">' + data.foodNutrients[3].nutrient.unitName + '</td></tr>');
+                nutrients.append('<tr class="nutrients-row"><td class="nutrients-cell">' + 'Sugars' + '</td><td class="nutrients-cell nutrients-cell-amount">' + parseFloat(data.foodNutrients[5].amount).toFixed(2) + '</td><td class="nutrients-cell nutrients-cell-unit">' + data.foodNutrients[5].nutrient.unitName + '</td></tr>');
+                nutrients.append('<tr class="nutrients-row"><td class="nutrients-cell">' + data.foodNutrients[4].nutrient.name + '</td><td class="nutrients-cell nutrients-cell-amount">' + parseFloat(data.foodNutrients[4].amount).toFixed(2) + '</td><td class="nutrients-cell nutrients-cell-unit">' + data.foodNutrients[4].nutrient.unitName + '</td></tr>');
             }
         });
     }
+    
     $(".submit").click(function() {
         if($(".input").val().length > 0) {
-            var input = $(".input").val();
+            let input = $(".input").val();
             if (!input.replace(/\s/g, '').length) {   
-                var errorText = 'You only pressed space!';
+                let errorText = 'You only pressed spacebar!';
                 error(errorText);
             }
             else {
@@ -50,30 +52,29 @@ $(function (){
                     success: function(data) {
                         results.empty();
                         results.append('<ul class="results-list"></ul>');
-                        var resultsList = $('.results-list');
+                        const resultsList = $('.results-list');
                         if(data.foods.length === 0) {
-                            var errorText = 'Did not find any results!';
+                            let errorText = 'Did not find any results!';
                             error(errorText);                            
                         }
-                        for(var counter = 0; counter < 10; counter++) {
-                            resultsList.append('<li class="result" value="' + counter + '">' + data.foods[counter].description + '</li>');
+                        for(let i = 0; i < 10; i++) {
+                            resultsList.append('<li class="result" value="' + i + '">' + data.foods[i].description + '</li>');
                         }
         
                         $(".result").click(function() {
-                            results.empty();
-                            var temp = $(this).val();
-                            specificFoodInfo(data.foods[temp].fdcId);
+                            let particularFoodId = $(this).val();
+                            specificFoodInfo(data.foods[particularFoodId].fdcId);
                         });
                     },
                     error: function() {
-                        var errorText = 'Could not connect to the server!';
+                        let errorText = 'Could not connect to the server!';
                         error(errorText);
                     }
                 }); 
             }
         }
         else {
-                var errorText = 'You did not write anything in searchbar!';
+                let errorText = 'You did not write anything in searchbar!';
                 error(errorText);
         }               
     });
