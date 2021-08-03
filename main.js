d4e58890db0d57a1861f3393eba2bcc85f247f61
FranciszekCzajka <franekczajkaa@gmail.com>
2021-08-03 17:31:52 +0200

    Remove jQuery and rewrite code to pure JS, does not have error handling

--- main.js
+++ main.js
@@ -1,95 +1,138 @@
-$(function (){
+const button = document.querySelector('.submit');
+let input = document.querySelector('.input');
 
-    const results = $('.results');
-
-    function error(errorText) {
-        const landing = $('.landing');
-        landing.after('<div class="connection-error"></div>')
-        const connectionError = $('.connection-error');
-        connectionError.append('<div class="connection-modal"></div>');
-        const connectionModal = $('.connection-modal');
-        connectionModal.append('<div class="connection-header"></div>');
-        connectionModal.append(`<div class="connection-message"><h4>${errorText}</h4></div>`);
-        const connectionHeader = $('.connection-header');
-        connectionHeader.append('<div class="connection-header-left">Error!</div><div class="connection-header-right"><div class="connection-x"><i class="fas fa-times"></i></div></div>');
-        $('.connection-x').click(function() {
-            connectionError.remove();
-        });
-    }
+function displayNutrients(foodID) {
+    const API_KEY = "nBKTTixEru2BA0nzf0DBnapEY5Tc6iERJ3lXpUU8";
+    const API_URL = "https://api.nal.usda.gov";
+    const NUTRIENTS_URL = `${API_URL}/fdc/v1/food/${foodID}?nutrients=203&nutrients=204&nutrients=205&nutrients=208&nutrients=291&nutrients=269&api_key=${API_KEY}`;
     
-    function specificFoodInfo(ID) {
-        results.empty().hide();
-        $.ajax({
-            type: 'GET',
-            url: `https://api.nal.usda.gov/fdc/v1/food/${ID}?nutrients=203&nutrients=204&nutrients=205&nutrients=208&nutrients=291&nutrients=269&api_key=nBKTTixEru2BA0nzf0DBnapEY5Tc6iERJ3lXpUU8`,
-            success: function(data) {
-                results.fadeIn(300);
-                results.append('<h3 class="food-name">Food: ' + data.description + '</h3>');
-                results.append('<table class="nutrients"></table>');
-                const nutrients = $('.nutrients');
-                nutrients.append(`<tr class="nutrients-row"><th class="nutrients-header">Name</th><th class="nutrients-header">Amount (per 100g)</th><th class="nutrients-header">Unit</th></tr>`);
-                nutrients.append(`<tr class="nutrients-row"><td class="nutrients-cell">${data.foodNutrients[0].nutrient.name}</td><td class="nutrients-cell nutrients-cell-amount">${data.foodNutrients[0].amount}</td><td class="nutrients-cell nutrients-cell-unit">${data.foodNutrients[0].nutrient.unitName}</td></tr>`);
-                nutrients.append(`<tr class="nutrients-row"><td class="nutrients-cell">${data.foodNutrients[1].nutrient.name}s</td><td class="nutrients-cell nutrients-cell-amount">${parseFloat(data.foodNutrients[1].amount).toFixed(2)}</td><td class="nutrients-cell nutrients-cell-unit">${data.foodNutrients[1].nutrient.unitName}</td></tr>`);
-                nutrients.append(`<tr class="nutrients-row"><td class="nutrients-cell">${data.foodNutrients[2].nutrient.name}</td><td class="nutrients-cell nutrients-cell-amount">${parseFloat(data.foodNutrients[2].amount).toFixed(2)}</td><td class="nutrients-cell nutrients-cell-unit">${data.foodNutrients[2].nutrient.unitName}</td></tr>`);
-                nutrients.append(`<tr class="nutrients-row"><td class="nutrients-cell">Carbohydrates</td><td class="nutrients-cell nutrients-cell-amount">${parseFloat(data.foodNutrients[3].amount).toFixed(2)}</td><td class="nutrients-cell nutrients-cell-unit">${data.foodNutrients[3].nutrient.unitName}</td></tr>`);
-                nutrients.append(`<tr class="nutrients-row"><td class="nutrients-cell">Sugars</td><td class="nutrients-cell nutrients-cell-amount">${parseFloat(data.foodNutrients[5].amount).toFixed(2)}</td><td class="nutrients-cell nutrients-cell-unit">${data.foodNutrients[5].nutrient.unitName}</td></tr>`);
-                nutrients.append(`<tr class="nutrients-row"><td class="nutrients-cell">${data.foodNutrients[4].nutrient.name}</td><td class="nutrients-cell nutrients-cell-amount">${parseFloat(data.foodNutrients[4].amount).toFixed(2)}</td><td class="nutrients-cell nutrients-cell-unit">${data.foodNutrients[4].nutrient.unitName}</td></tr>`);
-            },
-            error: function() {
-                let errorText = 'Could not connect to the server!';
-                error(errorText);
-            }
-        });
-    }
-    
-    $(".submit").click(function() {     
-        if($(".input").val().length > 0) {
-            let input = $(".input").val();
-            if (!input.replace(/\s/g, '').length) {   
-                let errorText = 'You only pressed spacebar!';
-                error(errorText)
-            }
-            else {
-                $.ajax({                
-                    type: 'GET',
-                    input: {"data": input},
-                    url: `https://api.nal.usda.gov/fdc/v1/foods/search?query=${input}&dataType=Survey%20%28FNDDS%29&pageSize=200&pageNumber=1&sortBy=dataType.keyword&api_key=nBKTTixEru2BA0nzf0DBnapEY5Tc6iERJ3lXpUU8`,
-                    success: function(data) {
+    const promise = fetch(NUTRIENTS_URL);
+    const foods = document.querySelector(".results");
 
-                        results.empty();
-                        
-                        if(data.foods.length === 0) {
-                            let errorText = 'Did not find any results!';
-                            error(errorText);
-                        }
-                        
-                        results.append('<ul class="results-list"></ul>');
-                        const resultsList = $('.results-list');
+    promise
+        .then(function(response) {
+            const processingPromise = response.json();
+            return processingPromise;
+        })
+        .then(function(processedResponse) {
+            const foodName = document.createElement("h3");
+            foodName.className = "food-name";
+            foodName.innerText = processedResponse.description;
+            foods.appendChild(foodName);
+            const nutrientsTable = document.createElement("table");
+            nutrientsTable.className = "nutrients";
+            foods.appendChild(nutrientsTable);
 
-                        for(let i = 0; i < 10; i++) {
-                            listElement = $(`<li tabIndex="${i}" class="result" value="${i}">${data.foods[i].description}</li>`);
-                            listElement.hide();
-                            resultsList.append(listElement);
-                            listElement.delay(`${i}00`).fadeIn(`${i}00`);
-                        }
-        
-                        $(".result").click(function() {
-                            let particularFoodId = $(this).val();
-                            results.empty();
-                            specificFoodInfo(data.foods[particularFoodId].fdcId);
-                        });
-                        
-                    },
-                    error: function() {
-                        let errorText = 'Could not connect to the server!';
-                        error(errorText);
+            const arrHeaders = ["Name", "Amount per 100g", "Unit"];
+
+            const tableHeadersRow = document.createElement("tr");
+            tableHeadersRow.className = "nutrients-row";
+
+            for(let i = 0; i < 3; i++) {
+                const tableHeader = document.createElement("th");
+                tableHeader.className = "nutrients-header";
+                tableHeader.innerText = arrHeaders[i];
+                tableHeadersRow.appendChild(tableHeader);
+            }
+
+            nutrientsTable.appendChild(tableHeadersRow);
+
+            for(let i = 0; i < 6; i++) {
+                const arrNutrients = [processedResponse.foodNutrients[i].nutrient.name, processedResponse.foodNutrients[i].amount, processedResponse.foodNutrients[i].nutrient.unitName]
+                const tableRow = document.createElement("tr");
+                tableRow.className = "nutrients-row";
+                nutrientsTable.appendChild(tableRow);
+
+                for(let j = 0; j < 3; j++) {
+                    const tableCell = document.createElement("td");
+
+                    if (arrNutrients[j] == "Sugars, total including NLEA") {
+                        arrNutrients[j] = "Sugars";
+                    } else if (arrNutrients[j] == "Carbohydrate, by difference") {
+                        arrNutrients[j] = "Carbohydrates";
                     }
-                }); 
+                    if (j == 1) {                        
+                        tableCell.className = "nutrients-cell nutrients-cell-amount";
+                        if (i > 0) {
+                            arrNutrients[j] = arrNutrients[j].toFixed(2)
+                            tableCell.innerText = arrNutrients[j];
+                        }
+                    } else if (j == 2) {
+                        tableCell.className = "nutrients-cell nutrients-cell-unit";
+                    } else {
+                        tableCell.className = "nutrients-cell";
+                    }
+
+                    tableCell.innerText = arrNutrients[j];
+                    tableRow.appendChild(tableCell)
+                }
             }
-        }
-        else {
-            let errorText = 'You did not write anything in searchbar!';
-            error(errorText);
-        }               
-    });
+        });
+}
+
+button.addEventListener('click', function() {
+    const API_KEY = "nBKTTixEru2BA0nzf0DBnapEY5Tc6iERJ3lXpUU8";
+    const API_URL = "https://api.nal.usda.gov";
+    const FOOD_URL = `${API_URL}/fdc/v1/foods/search?query=${input.value}&dataType=Survey%20%28FNDDS%29&pageSize=10&pageNumber=1&sortBy=dataType.keyword&api_key=${API_KEY}`;
+
+    const promise = fetch(FOOD_URL);
+    const foods = document.querySelector(".results");
+    
+    promise
+        .then(function(response) {
+            const processingPromise = response.json();
+            return processingPromise;
+        })
+        .then(function(processedResponse) {
+            foods.innerHTML = "";
+
+            const foodsList = document.createElement("ul");
+            foodsList.className = "results-list";
+            foods.appendChild(foodsList);
+
+            for(let i = 0; i < 10; i++) {
+                const food = document.createElement("li");
+                food.innerText = processedResponse.foods[i].description;
+                food.className = "result animation-fade-in";
+                food.value = i;
+                food.tabIndex = i;
+                foodsList.appendChild(food);
+            }
+
+            // Second method
+
+            /*for(let particularFood of processedResponse.foods) { 
+                console.log(particularFood);
+                const food = document.createElement("li");
+                food.innerText = particularFood.description;
+                food.className = "result animation-fade-in";
+                food.value = 1;
+                food.tabIndex = 1;
+                foodsList.appendChild(food);
+            }*/
+
+            //Event Delegation
+
+            foodsList.addEventListener('click', function(event) {
+                if (event.target.tagName == "LI") {
+                    input.value = "";
+                    foods.innerHTML = "";
+                    displayNutrients(processedResponse.foods[event.target.value].fdcId);
+                }
+            });
+
+            // Second method with forEach
+
+            /*results = document.querySelectorAll(".result");
+
+            results.forEach(function (particularFood) {
+                particularFood.addEventListener('click', function () {
+                    foods.innerHTML = "";
+                    displayNutrients(processedResponse.foods[particularFood.value].fdcId);
+                });
+            });*/
+            
+        });
 });
+
+
