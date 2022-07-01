c0cd0b6863c2a42a81192b005c213d0926065155
FranciszekCzajka <franekczajkaa@gmail.com>
2022-07-01 22:02:27 +0200

    Style for desktop

--- main.js
+++ main.js
@@ -7,13 +7,12 @@ function displayNutrients(food, results) {
 }
 
 function createFood() {
-
     const arrFoods = [];
 
     let idCounter = 0;
-    
+
     class FoodCreator {
-        constructor (foodObject) {
+        constructor(foodObject) {
             this.id = idCounter;
             this.name = foodObject.description;
             this.energy = foodObject.foodNutrients[3].value;
@@ -23,7 +22,7 @@ function createFood() {
             this.sugars = foodObject.foodNutrients[8].value;
             this.fiber = foodObject.foodNutrients[9].value;
         }
-        add (){ 
+        add() {
             arrFoods.push(this);
             idCounter++;
         }
@@ -33,8 +32,7 @@ function createFood() {
 
 const myNewFood = createFood();
 
-button.addEventListener('click', function() {
-    
+button.addEventListener("click", function () {
     const API_KEY = "nBKTTixEru2BA0nzf0DBnapEY5Tc6iERJ3lXpUU8";
     const API_URL = "https://api.nal.usda.gov";
     const FOOD_URL = `${API_URL}/fdc/v1/foods/search?query=${input.value}&dataType=Survey%20%28FNDDS%29&pageSize=10&pageNumber=1&sortBy=dataType.keyword&api_key=${API_KEY}`;
@@ -42,27 +40,32 @@ button.addEventListener('click', function() {
     const promise = fetch(FOOD_URL);
 
     promise
-        .then(function(response) {
+        .then(function (response) {
             const processingPromise = response.json();
             return processingPromise;
         })
-        .then(function(processedResponse) {
+        .then(function (processedResponse) {
             const foodsResponse = processedResponse.foods;
-            const foods = processedResponse.foods.map(function(food) {
+            const foods = processedResponse.foods.map(function (food) {
                 return food.description;
             });
             const foodList = document.querySelector(".food-list");
             const results = document.querySelector(".results");
             foodList.innerHTML = "";
-            foods.forEach(food => {
+            foods.forEach((food) => {
                 const listElement = document.createElement("li");
+                listElement.className = "food-list-element";
                 listElement.innerText = food;
                 foodList.append(listElement);
             });
 
-            foodList.addEventListener('click', function(foodFromList) {
-                if(foodFromList.target.tagName == "LI") {
-                    const food = new myNewFood(foodsResponse[foods.indexOf(foodFromList.target.innerText)]);
+            foodList.addEventListener("click", function (foodFromList) {
+                if (foodFromList.target.tagName == "LI") {
+                    const food = new myNewFood(
+                        foodsResponse[
+                            foods.indexOf(foodFromList.target.innerText)
+                        ]
+                    );
                     food.add();
                     displayNutrients(food, results);
                 }
